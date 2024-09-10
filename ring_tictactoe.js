aaa6bad74e54c12c9ba3d865330f6ed9074863b6
Lena <lennykulichkova@gmail.com>
2024-09-10 16:33:57 +0200

    set up a ring selection

--- ring_tictactoe.js
+++ ring_tictactoe.js
@@ -1,15 +1,19 @@
 
 class RingTicTacToe {
 	constructor() {
+		this.redPlayer = "red";
+		this.bluePlayer = "blue";
+		this.turn = null;
+
 		this.template = document.querySelector('.template');
-		this.redSide = document.querySelector('.side[data-color="0"]');
-		this.blueSide = document.querySelector('.side[data-color="1"]');
+		this.redSide = document.querySelector('.side[data-color="red"]');
+		this.blueSide = document.querySelector('.side[data-color="blue"]');
 		this.sides = [this.redSide, this.blueSide];
 		this.grid = document.querySelector('.grid');
 
 		this.cloneElements();
 		this.startGame();
-		this.restartButton.addEventListener('click', () => this.startGame());
+		// this.restartButton.addEventListener('click', () => this.startGame());
 	}
 
 	cloneElements() {
@@ -31,7 +35,34 @@ class RingTicTacToe {
 	}
 
 	startGame() {
+		this.sides.forEach(side => {
+			side.querySelectorAll('.cell').forEach(cell => {
+				cell.addEventListener('click', (e) => this.handleClick(e));
+			});
+		})
+	}
 
+	handleClick(e, clickAmount) {
+		const target = e.target;
+		const cell = target.closest('.cell');
+		const color = cell.getAttribute('data-color');
+		if (cell.querySelector('.ring[selected]') === null) {
+			cell.querySelector('.ring[data-ring="big"]').setAttribute('selected', '');
+			console.log('selected big');
+		} else if (cell.querySelector('.ring[data-ring="big"]').hasAttribute('selected')) {
+			cell.querySelector('.ring[data-ring="big"]').removeAttribute('selected', '');
+			cell.querySelector('.ring[data-ring="middle"]').setAttribute('selected', '');
+			console.log('selected middle');
+		} else if (cell.querySelector('.ring[data-ring="middle"]').hasAttribute('selected')) {
+			cell.querySelector('.ring[data-ring="middle"]').removeAttribute('selected', '');
+			cell.querySelector('.ring[data-ring="small"]').setAttribute('selected', '');
+			console.log('selected small');
+		} else if (cell.querySelector('.ring[data-ring="small"]').hasAttribute('selected')) {
+		cell.querySelector('.ring[data-ring="small"]').removeAttribute('selected', '');
+		console.log('selected small');
+	}
+		this.turn = color;
+		console.log('turn ' + this.turn);
 	}
 }
 
